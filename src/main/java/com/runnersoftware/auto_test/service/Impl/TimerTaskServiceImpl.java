bb1f85c8948885aa997805509017628d50307b05
9527 <hhy888444@gmail.com>
2021-05-19 20:39:50 +0800

    邮箱

--- src/main/java/com/runnersoftware/auto_test/service/Impl/TimerTaskServiceImpl.java
+++ src/main/java/com/runnersoftware/auto_test/service/Impl/TimerTaskServiceImpl.java
@@ -59,17 +59,14 @@ public class TimerTaskServiceImpl implements TimerTaskService {
         // 得到收件箱文件夹信息，获取邮件列表
         for (int i = 0; i < msgs.length; i++) {
             Message msg = msgs[i];
-            System.out.println("msg.getSubject() = " + msg.getSubject());
-            String email = msg.getFrom()[0].toString();
-            if (EmailProperties.SENDER.equals(email)){
-                //只查看未读邮件
-                if (!msg.getFlags().contains(Flags.Flag.SEEN)){
-                    Document document = Jsoup.parse(msg.getContent().toString());
-                    Bugs bugs = Bugs.buildDefault(document.text());
-                    bugsService.insert(bugs);
-                    //邮件已读
-                    msg.setFlag(Flags.Flag.SEEN, true);
-                }
+            //只查看未读邮件
+            if (!msg.getFlags().contains(Flags.Flag.SEEN)){
+                System.out.println("msg.getSubject() = " + msg.getSubject());
+                Document document = Jsoup.parse(msg.getContent().toString());
+                Bugs bugs = Bugs.buildDefault(document.text());
+                bugsService.insert(bugs);
+                //邮件已读
+                msg.setFlag(Flags.Flag.SEEN, true);
             }
 
         }
