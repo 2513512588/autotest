99aa4c9dc751946554f86582a251394b7d9fa195
9527 <hhy888444@gmail.com>
2021-05-21 17:08:05 +0800

    自启jenkins

--- src/main/java/com/runnersoftware/auto_test/AutoTestApplication.java
+++ src/main/java/com/runnersoftware/auto_test/AutoTestApplication.java
@@ -1,14 +1,30 @@
 package com.runnersoftware.auto_test;
 
+import com.runnersoftware.auto_test.service.ExecuteCmdService;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 
 @SpringBootApplication
-public class AutoTestApplication {
+public class AutoTestApplication implements InitializingBean {
+
+    @Autowired
+    private ExecuteCmdService executeCmdService;
+    @Value("${tomcat.path}")
+    private String tomcatPath;
 
     public static void main(String[] args) {
         SpringApplication.run(AutoTestApplication.class, args);
+
     }
 
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        executeCmdService.execCmd("cmd /c start startup.bat", new File(tomcatPath));
+    }
 }
